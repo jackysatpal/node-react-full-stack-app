0a1986d5f7ce769a27260e8e90675e8119d4fb59
Jacky Satpal <jackysatpal.212@gmail.com>
2018-03-04 11:53:43 -0500

    added cookie-session and google-oauth2.0

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
 const keys = require('./config/keys');
 require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+
+app.use(cookieSession({ 
+		maxAge: 30 * 24* 60 * 60 * 1000, 
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 	console.log(`server is up and running at ${PORT}`);
