0f9bc78bc8297f14606494c2fff8d7096821437f
Jacky Satpal <jackysatpal.212@gmail.com>
2018-03-03 22:15:27 -0500

    added google oauth2.0

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+const app = require('../index');
+
+module.exports = (app) => {
+	app.get('/', (req, res) => {
+		res.send('Full Stack app');
+	});
+
+	app.get('/auth/google', passport.authenticate('google', {
+			scope: ['profile', 'email']
+		})
+	);
+
+	app.get('/auth/google/callback', passport.authenticate('google'));
+};
